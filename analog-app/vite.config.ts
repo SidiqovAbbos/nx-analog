067139a6573db66f369684a83b5caa375308f424
Abbos Sidiqov <abbosidiqov@outlook.com>
2025-05-08 14:41:45 +0500

    refactor: update Vite config for consistency and readability

--- analog-app/vite.config.ts
+++ analog-app/vite.config.ts
@@ -1,59 +1,55 @@
 /// <reference types="vitest" />
 
-import analog from '@analogjs/platform';
-import { defineConfig, Plugin } from 'vite';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import analog from "@analogjs/platform";
+import { defineConfig, Plugin } from "vite";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 
 // @ts-expect-error @tailwindcss/vite currently uses mts. TypeScript is complaining this, but it works as expected.
-import tailwindcss from '@tailwindcss/vite';
-
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     root: __dirname,
     cacheDir: `../node_modules/.vite`,
-    
+
     ssr: {
-      noExternal: ['@analogjs/trpc','@trpc/server'],
+      noExternal: ["@analogjs/trpc", "@trpc/server"],
     },
-    
+
     build: {
-      outDir: '../dist/./analog-app/client',
+      outDir: "../dist/./analog-app/client",
       reportCompressedSize: true,
-      target: ['es2020'],
+      target: ["es2020"],
     },
     server: {
       fs: {
-        allow: ['.'],
+        allow: ["."],
       },
     },
     plugins: [
-      
       tailwindcss(),
-      
-      
       analog({
         nitro: {
+          preset: "cloudflare-pages",
           routeRules: {
-            '/': {
+            "/": {
               prerender: false,
-            }
-          }
-        }
+            },
+          },
+        },
       }),
-      
       nxViteTsPaths(),
     ],
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: ['src/test-setup.ts'],
-      include: ['**/*.spec.ts'],
-      reporters: ['default'],
+      environment: "jsdom",
+      setupFiles: ["src/test-setup.ts"],
+      include: ["**/*.spec.ts"],
+      reporters: ["default"],
     },
     define: {
-      'import.meta.vitest': mode !== 'production',
+      "import.meta.vitest": mode !== "production",
     },
   };
 });
